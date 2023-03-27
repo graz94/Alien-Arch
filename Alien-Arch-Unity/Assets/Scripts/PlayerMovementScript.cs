2e749b9fb4b8a2c4d12706bc708e85b0d3927900
graz94 <graz94enquiries@gmail.com>
2023-03-27 15:01:19 +0100

    Added Right and Jump Button

--- Alien-Arch-Unity/Assets/Scripts/PlayerMovementScript.cs
+++ Alien-Arch-Unity/Assets/Scripts/PlayerMovementScript.cs
@@ -9,18 +9,35 @@ public class PlayerMovementScript : MonoBehaviour
     //Set it to null by default, which means we do NOT have a reference yet
     private Rigidbody2D physicsBody = null;
 
+    public float jumpSpeed = 10;
     public void MoveLeft()
     {
         Vector2 newVelocity = physicsBody.velocity;
         newVelocity.x = -1;
         physicsBody.velocity = newVelocity;
     }
-
+    public void MoveRight()
+    {
+        Vector2 newVelocity = physicsBody.velocity;
+        newVelocity.x = 1;
+        physicsBody.velocity = newVelocity;
+    }
+    
+    public void Jump()
+    {
+        // Make a variable to hold our velocity and get the current velocity from the physics component
+        Vector2 newVelocity = physicsBody.velocity;
+        // Set our velocity to move in the positive y (up) direction
+        newVelocity.y = jumpSpeed;
+        // Update our component's velocity to be our newly changed value
+        physicsBody.velocity = newVelocity;
+    }
     private void Awake()
     {
         physicsBody = GetComponent<Rigidbody2D>();
     }
 
+    
     
     // Start is called before the first frame update
     void Start()
